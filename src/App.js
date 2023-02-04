feacbe620adf8bc2a93fe006764d4e19ae01c65e
meshi <mehhi@yandex.ru>
2023-02-04 15:27:45 +0300

    tiles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import AppRouter from './router/AppRouter';
 import { BrowserRouter } from 'react-router-dom';
+import PrimarySearchAppBar from './components/header/Header';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>Some header</header>
+      <PrimarySearchAppBar />
       <AppRouter />
     </BrowserRouter>
   );
--- src/components/tiles/TilesPage.jsx
+++ src/components/tiles/TilesPage.jsx
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchMoList } from '../../store/reducers/ActionCreators';
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+import Tile from './Tile'
+import TileModal from "./TileModal";
 
 
 export const TilesPage = () => {
@@ -16,6 +17,18 @@ export const TilesPage = () => {
   const {moList, isLoading, error} = useSelector(state => state.moListReducer);
   const currentMo = moList.filter(mo => mo.id === moList.currentMoId)[0];
 
+  // content for modal
+  const [content, setContent] = useState({content: 'first'});
+
+  //modal state
+  const [open, setOpen] = useState(false);
+  const handleOpen = (event) => 
+    {
+      setOpen(true);
+      setContent({content: event.target.id})
+    }
+  const handleClose = () => setOpen(false);
+
   useEffect(() => {
     clearHighchartsCredentials();
     dispatch(fetchMoList());
@@ -25,18 +38,19 @@ export const TilesPage = () => {
     <>
       <Grid container spacing={2}>
         <Grid item xs={8}>
-          <span style={{border: '1px solid black', display: 'block'}}>TEXT</span>
+          <Tile handleOpen={handleOpen} tileType="appoints"/>
         </Grid>
         <Grid item xs={4}>
-          <span style={{border: '1px solid black', display: 'block'}}>TEXT</span>
+        <Tile handleOpen={handleOpen} tileType="2"/>
         </Grid>
         <Grid item xs={4}>
-          <span style={{border: '1px solid black', display: 'block'}}>TEXT</span>
+        <Tile handleOpen={handleOpen} tileType="3"/>
         </Grid>
         <Grid item xs={8}>
-          <span style={{border: '1px solid black', display: 'block'}}>TEXT</span>
+        <Tile handleOpen={handleOpen} tileType="4"/>
         </Grid>
       </Grid>
+      <TileModal open={open} handleClose={handleClose} content={content}></TileModal>
     </>
   )
 }

eb3f39b8736238e1dd4e39c4c91681bcaf5253f6
m.podosenovmiac <podosenovmiac@yandex.com>
2023-02-07 18:03:00 +0300

    datepicker

--- src/components/admin/AdminPanel.jsx
+++ src/components/admin/AdminPanel.jsx
@@ -1,8 +1,41 @@
+import fileUploader from "../../utils/fileUploader";
+import dateConverter from "../../utils/dateConverter";
+import { Button } from "@mui/material";
+import { useState } from "react";
+
+
 const AdminPanel = () => {
+  const [file, setFile] = useState(null);
+  const [date, setDate] = useState(null);
+
+  const fileChangeHandler = (event) => {
+    const file = event.target.files[0];
+    setFile(file);
+  }
+
+  const uploadFile = async (event) => {
+    if (file) {
+      let reqResult = await fileUploader.uploadAppointmentFile(file);
+      console.log(reqResult);
+    }
+  }
+
+  const changeDateHandler = (event) => {
+    const chosenDate = new Date(event.target.value);
+    setDate(dateConverter.dateToStrForRequest(chosenDate))
+  }
+
   return(
-    <div>
-      ADMIN PANEL
-    </div>
+    <form className="upload-form" onSubmit={event => event.preventDefault()}>
+      <div className="upload__input_block">
+        <label htmlFor="ind-first-input">Выберите файл</label>
+        <input type="file" id="ind-first-input" onChange={fileChangeHandler}/>
+      </div>
+      <div className="upload__actions_block">
+        <Button onClick={uploadFile}/>
+      </div>
+      <input type="date" name="" id="" onChange={changeDateHandler}/>
+    </form>
   )
 }
 
---
+++ src/utils/fileUploader.js
@@ -0,0 +1,35 @@
+import { UPLOAD_BI_FILE_URL } from "../store/api_urls/apiUrls";
+import axios from "axios";
+
+class FileUploader {
+  constructor() {
+    this.appointmentFileUpURL = UPLOAD_BI_FILE_URL;
+  }
+
+  async uploadAppointmentFile (file, date) {
+    try {
+      const data = new FormData();
+      data.append('file', file);
+
+      const config = {
+        method: 'put',
+        url: this.appointmentFileUpURL,
+        headers: {
+          'Content-Disposition': `attachment; filename=${file.name}`,
+        },
+        params: { 
+          date: date,
+        },
+        data: data,
+      };
+
+      let response = await axios(config);
+
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export default new FileUploader();
